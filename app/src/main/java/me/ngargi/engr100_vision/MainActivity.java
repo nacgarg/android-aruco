75f4ac64d0fe333079cb610831a927fac805fb92
Nachiketa Gargi <nachiketa.gargi@gmail.com>
2019-04-14 20:30:09 -0400

    arduino serial comms working

--- app/src/main/java/me/ngargi/engr100_vision/MainActivity.java
+++ app/src/main/java/me/ngargi/engr100_vision/MainActivity.java
@@ -17,9 +17,18 @@ import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
 import org.opencv.imgproc.Imgproc;
 
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbManager;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
@@ -27,8 +36,28 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.view.View.OnTouchListener;
 import android.view.SurfaceView;
+import android.widget.TextView;
 
+
+import com.felhr.usbserial.UsbSerialDevice;
+import com.felhr.usbserial.UsbSerialInterface;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Vector;
 
 import CameraCalibration.CameraCalibrationActivity;
@@ -36,8 +65,13 @@ import es.ava.aruco.CameraParameters;
 import es.ava.aruco.Marker;
 import es.ava.aruco.MarkerDetector;
 
+import static aruco.min3d.Min3d.TAG;
+//import static com.felhr.usbserial.SerialPortBuilder.ACTION_USB_PERMISSION;
+
+
 public class MainActivity extends Activity implements OnTouchListener, CvCameraViewListener2 {
     private static final String  TAG              = "MainActivity";
+private String ACTION_USB_PERMISSION = "me.ngargi.engr100_vision.USB_PERMISSION";
 
     private boolean              mIsColorSelected = false;
     private Mat                  mRgba;
@@ -47,8 +81,39 @@ public class MainActivity extends Activity implements OnTouchListener, CvCameraV
     private CameraParameters cameraParameters;
     private VisionProcessThread visionProcess;
     private Thread processThread;
+    private TextView text;
+
+    private UsbSerialDevice serialPort;
+    private UsbManager usbManager;
+    private UsbDevice device;
+    private UsbDeviceConnection connection;
+    UsbSerialInterface.UsbReadCallback mCallback = new UsbSerialInterface.UsbReadCallback() { //Defining a Callback which triggers whenever data is read.
+        @Override
+        public void onReceivedData(byte[] arg0) {
+            String data = null;
+            try {
+                data = new String(arg0, "UTF-8");
+                data.concat("/n");
+                final String d = data;
+//                runOnUiThread(() -> text.append(d));
+
+
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+    };
+
+    public static final int SERVERPORT = 6000;
+
 
     private CameraBridgeViewBase mOpenCvCameraView;
+
+    private ServerSocket serverSocket;
+
+    Handler updateConversationHandler;
+
+    Thread serverThread = null;
 
     private BaseLoaderCallback  mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
@@ -76,6 +141,7 @@ public class MainActivity extends Activity implements OnTouchListener, CvCameraV
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        updateConversationHandler = new Handler();
         Log.i(TAG, "called onCreate");
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -83,12 +149,22 @@ public class MainActivity extends Activity implements OnTouchListener, CvCameraV
 
         setContentView(R.layout.activity_main);
 
+        text = findViewById(R.id.textView);
         mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.color_blob_detection_activity_surface_view);
         mOpenCvCameraView.setMaxFrameSize(1280, 960);
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
         mOpenCvCameraView.setCvCameraViewListener(this);
 
+//        this.serverThread = new Thread(new ServerThread());
+//        this.serverThread.start();
 
+        serialPort = null;
+        usbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(ACTION_USB_PERMISSION);
+        filter.addAction(UsbManager.ACTION_USB_DEVICE_ATTACHED);
+        filter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED);
+        registerReceiver(broadcastReceiver, filter);
     }
 
     @Override
@@ -129,6 +205,16 @@ public class MainActivity extends Activity implements OnTouchListener, CvCameraV
             mOpenCvCameraView.disableView();
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        try {
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void onCameraViewStarted(int width, int height) {
         mRgba = new Mat(height, width, CvType.CV_8UC4);
         DRAW_COLOR = new Scalar(255,0,0,255);
@@ -147,11 +233,87 @@ public class MainActivity extends Activity implements OnTouchListener, CvCameraV
         startActivity(i);
     }
 
+    public void checkUSB(View view) {
+        // todo
+        HashMap usbDevices = usbManager.getDeviceList();
+        if (!usbDevices.isEmpty()) {
+            boolean keep = true;
+            for (Object entry : usbDevices.keySet()) {
+                device  = (UsbDevice) usbDevices.get(entry);
+                int deviceVID = device.getVendorId();
+                text.append(device.getDeviceName() + " - " +Integer.toString(deviceVID) + "\n\n");
+//                showDialog(MainActivity.this, "title", Integer.toString(deviceVID), "OK", "Cancel");
+                if (deviceVID == 0x1FFB)//Arduino Vendor ID
+                {
+                    PendingIntent pi = PendingIntent.getBroadcast(this, 0,
+                            new Intent(ACTION_USB_PERMISSION), 0);
+                    usbManager.requestPermission(device, pi);
+                    keep = false;
+                } else {
+                    connection = null;
+                    device = null;
+                }
+
+                if (!keep)
+                    break;
+            }
+        }
+    }
+
+    private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() { //Broadcast Receiver to automatically start and stop the Serial connection.
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(ACTION_USB_PERMISSION)) {
+                boolean granted =
+                        intent.getExtras().getBoolean(UsbManager.EXTRA_PERMISSION_GRANTED);
+                if (granted) {
+                    connection = usbManager.openDevice(device);
+                    serialPort = UsbSerialDevice.createUsbSerialDevice(device, connection);
+                    if (serialPort != null) {
+                        if (serialPort.open()) { //Set Serial Connection Parameters.
+//                            setUiEnabled(true); //Enable Buttons in UI
+                            serialPort.setBaudRate(9600);
+                            serialPort.setDataBits(UsbSerialInterface.DATA_BITS_8);
+                            serialPort.setStopBits(UsbSerialInterface.STOP_BITS_1);
+                            serialPort.setParity(UsbSerialInterface.PARITY_NONE);
+                            serialPort.setFlowControl(UsbSerialInterface.FLOW_CONTROL_OFF);
+                            serialPort.read(mCallback); //
+                            text.append("Serial connection established!");
+//                            runOnUiThread(new updateUIThread("Serial connection established!"));
+//                            tvAppend(textView,"Serial Connection Opened!\n");
+
+                        } else {
+                            Log.d("SERIAL", "PORT NOT OPEN");
+                            text.append("port not open");
+
+                        }
+                    } else {
+                        Log.d("SERIAL", "PORT IS NULL");
+                        text.append("port is null");
+
+                    }
+                } else {
+                    Log.d("SERIAL", "PERM NOT GRANTED");
+                    text.append("no perm");
+
+                }
+            } else if (intent.getAction().equals(UsbManager.ACTION_USB_DEVICE_ATTACHED)) {
+                checkUSB(findViewById(R.id.usb_button));
+            } else if (intent.getAction().equals(UsbManager.ACTION_USB_DEVICE_DETACHED)) {
+//                onClickStop(stopButton);
+            }
+        };
+    };
+
     public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
         long t = System.currentTimeMillis();
         mRgba = inputFrame.rgba();
         visionProcess.setInput(mRgba);
         visionProcess.getOutput(mRgba);
+//        runOnUiThread();
+        Thread a = new Thread(new sendSerialThread(getBaseContext(), visionProcess.getSerialBytes(), serialPort));
+        a.start();
+        runOnUiThread(new updateUIThread(visionProcess.getPositionStr()));
         return mRgba;
     }
 
@@ -161,5 +323,131 @@ public class MainActivity extends Activity implements OnTouchListener, CvCameraV
         Imgproc.cvtColor(pointMatHsv, pointMatRgba, Imgproc.COLOR_HSV2RGB_FULL, 4);
 
         return new Scalar(pointMatRgba.get(0, 0));
+    }
+
+    class ServerThread implements Runnable {
+
+        public void run() {
+            Socket socket = null;
+            try {
+                serverSocket = new ServerSocket(SERVERPORT);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            while (!Thread.currentThread().isInterrupted()) {
+
+                try {
+
+                    socket = serverSocket.accept();
+
+                    CommunicationThread commThread = new CommunicationThread(socket);
+                    new Thread(commThread).start();
+
+                } catch (IOException e) {
+                   // e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    class CommunicationThread implements Runnable {
+
+        private Socket clientSocket;
+
+        private BufferedReader input;
+
+        public CommunicationThread(Socket clientSocket) {
+
+            this.clientSocket = clientSocket;
+
+            try {
+
+                this.input = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+
+                String read = input.readLine();
+                BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+                out.append(visionProcess.getPositionStr());
+                out.flush();
+                out.close();
+//                    updateConversationHandler.post(new updateUIThread(read));
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void run() {
+
+//            while (!Thread.currentThread().isInterrupted()) {
+//                if (clientSocket.isClosed()) {
+//                    continue;
+//                }
+//
+//
+//            }
+        }
+
+    }
+
+    class sendSerialThread implements Runnable {
+        private UsbSerialDevice serial;
+        private VisionProcessThread visionProcess;
+        private byte[] bytes;
+        public sendSerialThread(Context ctx, byte[] toSend, UsbSerialDevice s) {
+            this.serial = s;
+            this.bytes = toSend;
+
+        }
+
+        @Override
+        public void run() {
+            if (serial == null) {
+                return;
+            }
+            serial.write(bytes);
+        }
+    }
+
+    class updateUIThread implements Runnable {
+        private String msg;
+
+        public updateUIThread(String str) {
+            this.msg = str;
+        }
+
+        @Override
+        public void run() {
+//            msg += "\n Local IP: " + getIpAddress(getApplicationContext());
+//            text.setText(msg);
+        }
+    }
+
+    public static String getIpAddress(Context context) {
+        WifiManager wifiManager = (WifiManager) context.getApplicationContext()
+                .getSystemService(WIFI_SERVICE);
+
+        String ipAddress = intToInetAddress(wifiManager.getDhcpInfo().ipAddress).toString();
+
+        ipAddress = ipAddress.substring(1);
+
+        return ipAddress;
+    }
+
+    public static InetAddress intToInetAddress(int hostAddress) throws AssertionError{
+        byte[] addressBytes = { (byte)(0xff & hostAddress),
+                (byte)(0xff & (hostAddress >> 8)),
+                (byte)(0xff & (hostAddress >> 16)),
+                (byte)(0xff & (hostAddress >> 24)) };
+
+        try {
+            return InetAddress.getByAddress(addressBytes);
+        } catch (UnknownHostException e) {
+            throw new AssertionError();
+        }
     }
 }
--- app/src/main/java/me/ngargi/engr100_vision/VisionProcessThread.java
+++ app/src/main/java/me/ngargi/engr100_vision/VisionProcessThread.java
@@ -1,5 +1,9 @@
 package me.ngargi.engr100_vision;
 
+import android.content.Context;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbManager;
+import android.net.wifi.WifiManager;
 import android.util.Log;
 
 import org.opencv.core.Core;
@@ -7,7 +11,12 @@ import org.opencv.core.Mat;
 import org.opencv.core.Point;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.utils.Converters;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.List;
 import java.util.Vector;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -15,7 +24,13 @@ import java.util.concurrent.locks.ReentrantLock;
 import es.ava.aruco.CameraParameters;
 import es.ava.aruco.Marker;
 import es.ava.aruco.MarkerDetector;
+//import com.hoho.android.usbserial.*;
+//import com.hoho.android.usbserial.driver.UsbSerialDriver;
+//import com.hoho.android.usbserial.driver.UsbSerialProber;
+//import com.hoho.android.usbserial.driver.UsbSerialDriver;
 
+
+import static android.content.Context.WIFI_SERVICE;
 import static aruco.min3d.Min3d.TAG;
 
 public class VisionProcessThread implements Runnable {
@@ -24,6 +39,9 @@ public class VisionProcessThread implements Runnable {
         input = new Mat();
         output = new Mat();
         mDetector = new MarkerDetector();
+        detectedMarkers = new Vector<>();
+
+
 //        mDetector.setThresholdParams(9,9);
 //        mDetector.setThresholdMethod(MarkerDetector.thresSuppMethod.CANNY);
     }
@@ -31,10 +49,13 @@ public class VisionProcessThread implements Runnable {
     private final Lock inmatlock = new ReentrantLock(true);
     private CameraParameters cameraParameters;
     public Mat input;
+    private Vector<Marker> detectedMarkers;
+    private Lock markerLock = new ReentrantLock(false);
     public Mat output;
     private Lock outmatlock = new ReentrantLock(true);
     public boolean exit = false;
     private MarkerDetector mDetector;
+    private byte[] lastSerialData;
 
 
     public void setInput(Mat frame) {
@@ -55,6 +76,61 @@ public class VisionProcessThread implements Runnable {
         outmatlock.unlock();
     }
 
+    public String getPositionStr() {
+        String output = "";
+        markerLock.lock();
+        // Format for marker: id rvec1 rvec2 rvec3 tvec1 tvec2 tvec3
+        for (Marker m : detectedMarkers) {
+            output += "id: " + m.getMarkerId() + "\n\n";
+            java.util.List<java.lang.Double> rvec = new Vector<>();
+            java.util.List<java.lang.Double> tvec = new Vector<>();
+
+            Converters.Mat_to_vector_double(m.getRvec(), rvec);
+            Converters.Mat_to_vector_double(m.getTvec(), tvec);
+
+
+//            for (double r : rvec) {
+//                output += r+" ";
+//            }
+
+
+            double x = ((Vector<Double>) tvec).elementAt(0);
+            double y = ((Vector<Double>) tvec).elementAt(1);
+            double z = ((Vector<Double>) tvec).elementAt(2);
+
+            double vert_rad = Math.atan2(z, y);
+            double hori_rad = Math.atan2(z, x);
+
+            output += "Vertical: " + ((360 / 6.28 * vert_rad)) + "\n";
+            output += "Horizontal: " + ((360 / 6.28 * hori_rad)) + "\n";
+
+//            for (double r : tvec) {
+//                output += r+"\n";
+//            }
+            output += "\n";
+        }
+        markerLock.unlock();
+        return output;
+    }
+
+    public byte[] getSerialBytes() {
+        // Output is 2 bytes. First byte is the position of the pan servo, second byte is the position of the tilt servo
+        if (!detectedMarkers.isEmpty()) {
+            java.util.List<java.lang.Double> tvec = new Vector<>();
+            Converters.Mat_to_vector_double(detectedMarkers.firstElement().getTvec(), tvec);
+            double x = ((Vector<Double>) tvec).elementAt(0);
+            double y = ((Vector<Double>) tvec).elementAt(1);
+            double z = ((Vector<Double>) tvec).elementAt(2);
+
+            double vert_rad = Math.atan2(z, y);
+            double hori_rad = Math.atan2(z, x);
+
+            byte x_byte = (byte) (255 - (int) (127 + (hori_rad / 6.28 * 255)));
+            byte y_byte = (byte) (int) (vert_rad / 6.28 * 255);
+            lastSerialData = new byte[] {x_byte, y_byte};
+        }
+        return lastSerialData;
+    }
 
     @Override
     public void run() {
@@ -69,8 +145,7 @@ public class VisionProcessThread implements Runnable {
 
             if (cameraParameters.isValid()) {
                 long dt = System.currentTimeMillis();
-
-                Vector<Marker> detectedMarkers = new Vector<>();
+                markerLock.lock();
                 mDetector.detect(mRgba, detectedMarkers, cameraParameters, 0.1f);
                 Log.e(TAG, "Detection Time: " + (System.currentTimeMillis() - dt));
                 Log.e(TAG, mRgba.width() + " by " + mRgba.height() );
@@ -81,6 +156,8 @@ public class VisionProcessThread implements Runnable {
                     m.draw3dAxis(mRgba, cameraParameters, new Scalar(0, 255, 0));
                     m.draw3dCube(mRgba, cameraParameters, new Scalar(0, 255, 0));
                 }
+                markerLock.unlock();
+
 
                 Log.e(TAG, "Marker count: " + detectedMarkers.size());
             } else {
@@ -90,6 +167,7 @@ public class VisionProcessThread implements Runnable {
             outmatlock.lock();
             mRgba.copyTo(output);
             outmatlock.unlock();
+
         }
 
 
